cb3a0340c89e43681b551275813d207c70c0c02b
bigzinxb <bigzin369@gmail.com>
2023-12-11 21:42:16 -0300

    00

--- lib/functions_bit.rb
+++ lib/functions_bit.rb
@@ -1,13 +1,14 @@
 module Bit
-    class self.FixedFunctionsBits
+    class FixedFunctionsBits
         def reversed(bit_str)
+            left = 0
+            rigth = (bit_str.length - 1)
+
+            str_reversed = []
+
             if bit_str.length < 2
                 return bit_str
             end
-
-            left = 0
-            rigth = (bit_str.length - 1)
-            str_reversed = []
 
             until left > rigth
                 str_reversed[left] = bit_str[rigth]
@@ -16,28 +17,29 @@ module Bit
                 left += 1
                 rigth -= 1
             end
+
             return str_reversed.reduce(:+)
         end
     end
 
     def self.sum_bits(bit_1, bit_2)
-        reversed_str = FunctionsBits.new
+        reversed_str = FixedFunctionsBits.new
 
         result = bit_reverse_1 = reversed_str.reversed(bit_1)
-        bit_reverse_2 = reverse_str.reversed(bit_2)
+        bit_reverse_2 = reversed_str.reversed(bit_2)
 
         (0...bit_reverse_1.length).each do |bit1|
             bit2 = bit1
 
-            if bit_reverse_1[i] == "1" && bit_reverse_2[bit2] == "1"
-                result[i + 1] = "1"
-                resutl[i] = "0"
-            elsif bit_reverse_1[i] == "1" && bit_reverse2[bit2] == "0"
-                result[i] = "1"
-            elsif bit_reverse_1[i] == "0" && bit_reverse2[bit2]
-                result[i] = "1"
+            if bit_reverse_1[bit1] == "1" && bit_reverse_2[bit2] == "1"
+                result[bit1 + 1] = "1"
+                result[bit1] = "0"
+            elsif bit_reverse_1[bit1] == "1" && bit_reverse_2[bit2] == "0"
+                result[bit1] = "1"
+            elsif bit_reverse_1[bit1] == "0" && bit_reverse_2[bit2] == "1"
+                result[bit1] = "1"
             end
         end
-        return reversed(result)
+        return reversed_str.reversed(result)
     end
 end
